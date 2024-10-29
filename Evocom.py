98c56e1b7144858a65bc54f4dbd49246a04f19a6
niknursyarwani <102133391+niknursyarwani@users.noreply.github.com>
2024-10-29 09:28:16 +0800

    Update Evocom.py

--- Evocom.py
+++ Evocom.py
@@ -14,7 +14,8 @@ POP_SIZE = 500
 MUT_RATE = 0.2
 
 #TARGET: Our goal.
-TARGET = 'Syarwani'
+#TARGET = 'Syarwani'
+TARGET = st.text_input("Enter your name", "Syarwani")
 
 #GENES: Options from which our population would be created.
 GENES = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
