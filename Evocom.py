ea0a1066cbf006ca5b5855210a642173e6964b5d
niknursyarwani <102133391+niknursyarwani@users.noreply.github.com>
2024-11-03 12:26:46 +0800

    Update Evocom.py

--- Evocom.py
+++ Evocom.py
@@ -2,17 +2,14 @@ import streamlit as st
 st.set_page_config(
   page_title="Genetic Algorithm"
 )
-
 st.header("Genetic Algorithm", divider="gray")
 
 import random
 
 #POP_SIZE: Number of Chromosomes in our list.
 POP_SIZE = 500
-
 #MUT_RATE: Rate at which our string will be changed.
 MUT_RATE = 0.2
-
 #TARGET: Our goal.
 #TARGET = 'Syarwani'
 TARGET = st.text_input("Enter your name","Syarwani")
